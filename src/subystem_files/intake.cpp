e604fc9456c041a805b2defba8634595b3a7b26a
devmam999 <devmam999@gmail.com>
2024-07-20 22:08:18 -0700

    hopefully improved efficiency

--- src/subystem_files/intake.cpp
+++ src/subystem_files/intake.cpp
@@ -1,23 +1,17 @@
 #include "main.h"
 
-int intakePower = 0;
 bool intakeOn = false;
 bool outtakeOn = false;
+int direction = 0;
 
 void Intake() {
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
-        intakeOn = !intakeOn;
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2))
-        outtakeOn = !outtakeOn;
-    if (intakeOn) {
-        intakePower = 127;
-    }
-    else {
-        if (outtakeOn)
-            intakePower = -127;
-        else {
-            intakePower = 0;
-        }        
-    }
-    intake.move(intakePower);
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) intakeOn = !intakeOn;
+
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) outtakeOn = !outtakeOn;
+
+    if (outtakeOn) direction = 1;
+    
+    else direction = -1;
+
+    intake.move(127*intakeOn*direction);
 }
