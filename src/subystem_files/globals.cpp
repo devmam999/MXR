d70f00be027c262888b34e0a2dffd9f0fd87cbcc
devmam999 <devmam999@gmail.com>
2024-07-20 21:39:24 -0700

    updated ports

--- src/subystem_files/globals.cpp
+++ src/subystem_files/globals.cpp
@@ -1,23 +1,23 @@
 #include "main.h"
 
 // Motors
-pros::Motor drive_left_1(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor drive_left_2(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor drive_left_3(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_left_1(13, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_left_2(14, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_left_3(15, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor_Group drive_left({drive_left_1, drive_left_2, drive_left_3});
 
-pros::Motor drive_right_1(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor drive_right_2(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor drive_right_3(6, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_right_1(16, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_right_2(18, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor drive_right_3(19, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor_Group drive_right({drive_right_1, drive_right_2, drive_right_3});
 
 // Intake
-pros::Motor intake_1(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intake_2(9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake_1(1, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake_2(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor_Group intake({intake_1, intake_2});
 
 // Soleniold
-pros::ADIDigitalOut soleniold(8, false);
+pros::ADIDigitalOut soleniold('A', false);
 
 // Controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
