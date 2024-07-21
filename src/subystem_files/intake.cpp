36e991901abf886fd0e3f7c00ac9d395b406d67b
devmam999 <107886293+devmam999@users.noreply.github.com>
2024-07-21 06:31:57 -0700

    Fixed my stupidity

--- src/subystem_files/intake.cpp
+++ src/subystem_files/intake.cpp
@@ -9,9 +9,9 @@ void Intake() {
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) outtakeOn = !outtakeOn;
 
-    if (outtakeOn) direction = 1;
+    if (outtakeOn) direction = -1;
     
-    else direction = -1;
+    else direction = 1;
 
     intake.move(127*intakeOn*direction);
 }
