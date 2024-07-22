b9bd073d040e4b4a2159e49e3837d7bec5635557
devmam999 <devmam999@gmail.com>
2024-07-22 15:17:18 -0700

    updated ports again

--- src/subystem_files/globals.cpp
+++ src/subystem_files/globals.cpp
@@ -12,8 +12,8 @@ pros::Motor drive_right_3(19, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCO
 pros::Motor_Group drive_right({drive_right_1, drive_right_2, drive_right_3});
 
 // Intake
-pros::Motor intake_1(1, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intake_2(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake_1(2, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake_2(10, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor_Group intake({intake_1, intake_2});
 
 // Solenoid
